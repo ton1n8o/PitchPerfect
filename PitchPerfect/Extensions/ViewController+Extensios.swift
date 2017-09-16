391249efcb76d9b2ba983844f22d17263b1b81d1
Antônio Carlos <antoniocarlos.dev@gmail.com>
2017-09-16 09:47:16 +0800

    tiny adjustment

--- PitchPerfect/Extensions/ViewController+Extensios.swift
+++ PitchPerfect/Extensions/ViewController+Extensios.swift
@@ -29,7 +29,7 @@ extension UIViewController {
     func showAlert(_ title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
 }
